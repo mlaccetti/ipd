d61649fd78553739e32b71d436bba5404f652d47
Michael Laccetti <michael@laccetti.com>
2018-06-13 20:08:47 -0400

    Made the help text nicer

--- internal/util/config.go
+++ internal/util/config.go
@@ -1,47 +1,52 @@
 package util
 
 import (
+	"bytes"
 	"flag"
+	"os"
+	"text/template"
 
 	flags "github.com/spf13/pflag"
 
 	"github.com/spf13/viper"
 )
 
-type opts struct {
-	Help          bool
-	Verbose       bool
-	CountryDBPath string
-	CityDBPath    string
-	Listen        string
-	ListenTLS     string
-	TLSKey        string
-	TLSCert       string
-	ReverseLookup bool
-	PortLookup    bool
-	Template      string
-	IPHeader      string
+type HelpTemplate struct {
+	Flags string
 }
 
-var _ = &opts{
-	Help: *flags.BoolP("help", "h", false, "This help text"),
-	Verbose: *flags.BoolP("verbose", "v", false, "verbose output (default false"),
-	CountryDBPath: *flags.StringP("country-db", "f", "", "Path to GeoIP country database"),
-	CityDBPath: *flags.StringP("city-db", "c", "", "Path to GeoIP city database"),
-	Listen: *flags.StringP("listen", "l", ":8080", "Listening address"),
-	ListenTLS: *flags.StringP("listen-tls", "s", ":8443", "Listening address for TLS"),
-	TLSKey: *flags.StringP("tls-key", "k", "", "Path to the TLS key to use (ignored if no TLS listen address is specified)"),
-	TLSCert: *flags.StringP("tls-cert", "e", "", "Path to the TLS certificate to use (ignored if no TLS listen address is specified)"),
-	ReverseLookup: *flags.BoolP("reverse-lookup", "r", true, "Perform reverse hostname lookups"),
-	PortLookup: *flags.BoolP("port-lookup", "p", true, "Perform port lookups"),
-	Template: *flags.StringP("template", "t", "index.html", "Path to template"),
-	IPHeader: *flags.StringP("trusted-header", "H", "X-Forwarded-For", "Header with 'real' IP, if present"),
-}
+const helpTemplate = `
+Usage:
+  ipd2 [OPTIONS]
+
+Application Options:
+{{.Flags}}
+
+Help Options:
+  -h, --help                    Show this help message
+`
+
+var buf = new(bytes.Buffer)
 
 func Config() (*viper.Viper ) {
+	_ = *flags.BoolP("help", "h", false, "Show this help message")
+	_ = *flags.BoolP("verbose", "v", false, "Verbose output (default false")
+	_ = *flags.StringP("listen", "l", ":8080", "Listening address")
+	_ = *flags.StringP("listen-tls", "s", ":8443", "Listening address for TLS")
+	_ = *flags.StringP("tls-key", "k", "", "Path to the TLS key to use (ignored if no TLS listen address is specified)")
+	_ = *flags.StringP("tls-cert", "e", "", "Path to the TLS certificate to use (ignored if no TLS listen address is specified)")
+	_ = *flags.StringP("country-db", "f", "", "Path to GeoIP country database")
+	_ = *flags.StringP("city-db", "c", "", "Path to GeoIP city database")
+	_ = *flags.BoolP("port-lookup", "p", true, "Perform port lookups")
+	_ = *flags.BoolP("reverse-lookup", "r", true, "Perform reverse hostname lookups")
+	_ = *flags.StringP("template", "t", "index.html", "Path to template")
+	_ = *flags.StringP("trusted-header", "H", "X-Forwarded-For", "Header with 'real' IP, if present")
+
 	v := viper.New()
 
 	flags.CommandLine.SortFlags = false
+	flags.CommandLine.MarkHidden("help")
+	flags.CommandLine.SetOutput(buf)
 
 	flags.CommandLine.AddGoFlagSet(flag.CommandLine)
 	flags.Parse()
@@ -53,5 +58,10 @@ func Config() (*viper.Viper ) {
 }
 
 func PrintHelp() {
-	flags.PrintDefaults()
+	flags.CommandLine.PrintDefaults()
+	data := HelpTemplate{buf.String()}
+	buf = new(bytes.Buffer)
+
+	t := template.Must(template.New("help").Parse(helpTemplate))
+	t.Execute(os.Stdout, data)
 }
